f5473449f41a1cbba7ba13d473b5d68968b73b25
MARJAN305 <marvjantjies3@gmail.com>
2024-03-18 10:51:27 +0200

    IWA2 Challenge 3 1. Added two const for warn and info to be logged with their respective msgs. 2. Double quotes should be single quotes.

---
+++ IWA2 Submission/Challenge 3/jscript.js
@@ -0,0 +1,16 @@
+/**
+ * It is important to show the following message in order to:
+ *  - to assure users
+ *  - to scare hackers
+ *  - to impress investors
+ */
+
+//added two const for warn and info to be logged with their respective msgs
+const warn = 'Security scan starting'
+const info = 'Please wait for scan to complete before closing the browser'
+
+console.warn('Security scan starting')
+
+/* It is important to let user know when they can close the page  */
+//double quotes should be single quotes
+console.info('Please wait for scan to complete before closing the browser')
