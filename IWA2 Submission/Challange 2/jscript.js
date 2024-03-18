4eb44f85914900e7cd06a1b96d0502bc1fd8c9f1
MARJAN305 <marvjantjies3@gmail.com>
2024-03-18 10:15:32 +0200

    IWA2 Challange2 1.I just changed the comment tool used. 2. It should be console.log

--- IWA2 Submission/Challange 2/jscript.js
+++ IWA2 Submission/Challange 2/jscript.js
@@ -1,6 +1,8 @@
-/** (c) ACME Inc. 2010 */
+//i just changed the comment tool used
+//(c) ACME Inc. 2010
 
-/** This is the date that a user created their account */
+//This is the date that a user created their account
 const date = '10/07/2014'
 
+//it should be console.log
 console.log(date)
